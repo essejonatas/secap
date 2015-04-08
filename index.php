1ae952002d75878e34587635dbfc09993d5904e8
essejonatas <essejonatas@gmail.com>
2015-04-08 02:07:42 -0300

    adicionando conteudo

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Rede SECAP</title>
+	<link rel="stylesheet" type="text/css" href="css/libs.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="logo-site">
+		<img src="img/logo-secap.jpg">
+	</div>
+
+	<div class="slider">
+		<ul id="owl-slider" class="owl-carousel owl-theme">
+		  <li class="slider-item"><img src="img/slider.jpg" alt="estudantes"></li>
+		  <!-- <li class="slider-item"><img src="img/slider2.jpg" alt="empresários"></li> -->
+		</ul>
+	</div>
+
+	<div class="apresentacao">
+		<div class="grid">
+			<h1 class="apresentacao-titulo">REDE SECAP</h1>
+			<p class="apresentacao-texto">O Seminário de Capacitação Profissional (SECAP) surgiu em 2011 do desejo voluntário de jovens empreendedores que assumiram o desafio de repassar a certeza do desenvolvimento profissional através da educação.</p>
+			<p class="apresentacao-texto">Seminário SECAP cresceu e transformou-se em REDE SECAP, que engloba uma série de ações como: palestras, um programa semanal na rádio Nordeste AM 900khz, e o CRER – Cristãos Relevantes, Empreendedores de Resultados, ambos com o objetivo de alertar para a importância da educação como alavanca para mudanças sociais, de forma que possam transformar a vida dos envolvidos através do exemplo e encorajamento a adoção de práticas empreendedoras.</p>
+		</div>
+	</div>
+
+	<div class="projetos">
+		<div class="grid">
+			<h2 class="projetos-titulo">NOSSOS PROJETOS</h2>
+			<ul>
+				<li class="projeto-item col-1-5">
+					<div class="projeto-item-logo"><img src=""></div>
+					<h3 class="projeto-item-titulo">CRER</h3>
+				</li>
+				<li class="projeto-item col-1-5">
+					<div class="projeto-item-logo"><img src=""></div>
+					<h3 class="projeto-item-titulo">SECAP</h3>
+				</li>
+				<li class="projeto-item col-1-5">
+					<div class="projeto-item-logo"><img src=""></div>
+					<h3 class="projeto-item-titulo">SECAP+</h3>
+				</li>
+				<li class="projeto-item col-1-5">
+					<div class="projeto-item-logo"><img src=""></div>
+					<h3 class="projeto-item-titulo">PROGRAMA REDE SECAP</h3>
+				</li>
+				<li class="projeto-item col-1-5">
+					<div class="projeto-item-logo"><img src=""></div>
+					<h3 class="projeto-item-titulo">PROGRAMA GERAÇÃO SABER</h3>
+				</li>
+			</ul>	
+		</div>
+	</div>
+
+	<div class="sobre">
+		<div class="grid">
+			<div class="col-1-2">
+				<div class="sobre-item">
+					<h2 class="sobre-item-titulo">MISSÃO</h2>
+					<p class="sobre-item-descricao">Compartilhar conhecimento e transformar vidas através da educação para valores.</p>
+				</div>
+				<div class="sobre-item">
+					<h2 class="sobre-item-titulo">VISÃO</h2>
+					<p class="sobre-item-descricao">Relacionamento como meio para transformação; Empreendedorismo social;  Inovação; Sustentabilidade; Conhecimento bíblico.</p>
+				</div>
+			</div>
+			<div class="col-1-2">
+				<div class="col-1-2">
+					<div class="sobre-item">
+					<h2 class="sobre-item-titulo">OBJETIVOS</h2>
+					<p class="sobre-item-descricao">Contribuir na orientação e qualificação de estudantes e profissionais através da disseminação do conhecimento, experiências e discussões sobre as tendências de mercado.</p>
+				</div>
+				<div class="col-1-2">
+					<img src="img/about-bg-right.jpg">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="contato">
+		<div class="grid">
+			<div class="col-8-12">
+				<div class="contato-ciade">
+					<img src="img/logo-ciade.jpg">
+					<p class="ciade-descricao">O CIADE, Centro integrado de assistência social da assembléia de Deus é uma instituição social Com Sede própria, recém-construída e inaugurada em julho de 2011, com endereço localizado na Rua Abreulândia nº 77 com a Rua Esplendor, no bairro Planalto, onde atualmente estão abrigadas 22 senhoras. Estas instalações ocupam um terreno com 2.000m2, e uma área construída de 1.133,90 m2. O quadro funcional de 34 pessoas é composto por profissionais nas áreas de:</p>
+				</div>
+			</div>
+			<div class="col-4-12">
+				<div class="contato-fomulario">
+					<h2 class="contato-fomulario-titulo">Entre em <span class="contato-formulario-titulo-destaque">Contato</span> conosco</h2>
+					<form>
+						<label for="nome">Nome:</label>
+						<input type="text" name="nome" class="contato-formulario-input" placeholder="Ex.: João Maria">
+						<label for="email">Email:</label>
+						<input type="email" name="email" class="contato-formulario-input" placeholder="Ex.: joao@mail.com">
+						<label for="mensagem">Mensagem:</label>
+						<textarea name="mensagem" class="contato-formulario-textarea" placeholder="Escreva sua mensagem aqui"></textarea>
+						<input type="submit" value="enviar">
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer>
+		<div class="grid">
+			<ul class="links">
+				<li class="links-item"><a href="http://facebook.com/redesecap">facebook</a></li>
+				<li class="links-item"><a href="http://facebook.com/redesecap">twitter</a></li>
+			</ul>
+		</div>
+	</footer>	
+	<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
