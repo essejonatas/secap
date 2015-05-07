2b02a212d133acc7ebe474614e74934e6a739e33
essejonatas <essejonatas@gmail.com>
2015-05-07 14:57:10 -0300

    adequações e remoção de pastas não pertencentes ao projeto

--- index.php
+++ index.php
@@ -8,52 +8,56 @@
 </head>
 <body>
 	<div class="logo-site">
-		<img src="img/logo-secap.jpg">
+		<img src="img/logo-secap.jpg" alt="logo secap">
 	</div>
 
 	<div class="slider">
-		<ul id="owl-slider" class="owl-carousel owl-theme">
-		  <li class="slider-item"><img src="img/slider.jpg" alt="estudantes"></li>
-		  <!-- <li class="slider-item"><img src="img/slider2.jpg" alt="empresários"></li> -->
-		</ul>
+		<div class="grid">
+			<h2 class="slider-titulo">Nós Acreditamos no <br> conhecimento</h2>
+			<h3 class="slider-descricao">Nossa missão é compartilhar conhecimento e transformar vidas através da educação para valores</h3>
+		</div>
 	</div>
 
 	<div class="apresentacao">
 		<div class="grid">
-			<h1 class="apresentacao-titulo">REDE SECAP</h1>
-			<p class="apresentacao-texto">O Seminário de Capacitação Profissional (SECAP) surgiu em 2011 do desejo voluntário de jovens empreendedores que assumiram o desafio de repassar a certeza do desenvolvimento profissional através da educação.</p>
-			<p class="apresentacao-texto">Seminário SECAP cresceu e transformou-se em REDE SECAP, que engloba uma série de ações como: palestras, um programa semanal na rádio Nordeste AM 900khz, e o CRER – Cristãos Relevantes, Empreendedores de Resultados, ambos com o objetivo de alertar para a importância da educação como alavanca para mudanças sociais, de forma que possam transformar a vida dos envolvidos através do exemplo e encorajamento a adoção de práticas empreendedoras.</p>
+			<h1 class="apresentacao-titulo">REDE <strong>SECAP<strong></h1>
+			<p class="apresentacao-texto">O Seminário de Capacitação Profissional (<strong>SECAP<strong>) surgiu em 2011 do desejo voluntário de jovens cristãos que assumiram o desafio de repassar a certeza do desenvolvimento profissional através da educação para valores. Desde 2013 fazemos parte da Ong <a style="color: #555;"href="http://www.ciade.org.br">CIADE</a>, que há mais de 30 anos desenvolve projetos sociais relevantes em na região metropolitana de Natal.</p>
+			<p class="apresentacao-texto">O <strong>SECAP<strong> cresceu e transformou-se em REDE SECAP, que engloba uma série de ações como: Palestras em Igrejas/Escolas Públicas, um Programa semanal na Rádio Nordeste AM 900khz, e o CRER – Cristãos Relevantes, Empreendedores de Resultados, ambos com o objetivo de alertar para a importância da educação como alavanca para mudanças sociais.</p>
 		</div>
 	</div>
+	
+	<div class="projects-top"></div>
 
 	<div class="projetos">
 		<div class="grid">
 			<h2 class="projetos-titulo">NOSSOS PROJETOS</h2>
 			<ul>
 				<li class="projeto-item col-1-5">
-					<div class="projeto-item-logo"><img src=""></div>
+					<div class="projeto-item-logo" id="logo-crer"></div>
 					<h3 class="projeto-item-titulo">CRER</h3>
 				</li>
 				<li class="projeto-item col-1-5">
-					<div class="projeto-item-logo"><img src=""></div>
+					<div class="projeto-item-logo" id="logo-secap"></div>
 					<h3 class="projeto-item-titulo">SECAP</h3>
 				</li>
 				<li class="projeto-item col-1-5">
-					<div class="projeto-item-logo"><img src=""></div>
+					<div class="projeto-item-logo" id="logo-secap_mais"></div>
 					<h3 class="projeto-item-titulo">SECAP+</h3>
 				</li>
 				<li class="projeto-item col-1-5">
-					<div class="projeto-item-logo"><img src=""></div>
+					<div class="projeto-item-logo" id="logo-radio"></div>
 					<h3 class="projeto-item-titulo">PROGRAMA REDE SECAP</h3>
 				</li>
 				<li class="projeto-item col-1-5">
-					<div class="projeto-item-logo"><img src=""></div>
+					<div class="projeto-item-logo" id="logo-geracao_saber"></div>
 					<h3 class="projeto-item-titulo">PROGRAMA GERAÇÃO SABER</h3>
 				</li>
 			</ul>	
 		</div>
 	</div>
 
+	<div class="projects-bottom"></div>
+	
 	<div class="sobre">
 		<div class="grid">
 			<div class="col-1-2">
@@ -62,7 +66,7 @@
 					<p class="sobre-item-descricao">Compartilhar conhecimento e transformar vidas através da educação para valores.</p>
 				</div>
 				<div class="sobre-item">
-					<h2 class="sobre-item-titulo">VISÃO</h2>
+					<h2 class="sobre-item-titulo">VALORES</h2>
 					<p class="sobre-item-descricao">Relacionamento como meio para transformação; Empreendedorismo social;  Inovação; Sustentabilidade; Conhecimento bíblico.</p>
 				</div>
 			</div>
@@ -71,9 +75,12 @@
 					<div class="sobre-item">
 					<h2 class="sobre-item-titulo">OBJETIVOS</h2>
 					<p class="sobre-item-descricao">Contribuir na orientação e qualificação de estudantes e profissionais através da disseminação do conhecimento, experiências e discussões sobre as tendências de mercado.</p>
+					</div>
 				</div>
 				<div class="col-1-2">
-					<img src="img/about-bg-right.jpg">
+					<div class="sobre-imagem">
+						<img src="img/about-bg-right.jpg">
+					</div>
 				</div>
 			</div>
 		</div>
@@ -82,14 +89,39 @@
 	<div class="contato">
 		<div class="grid">
 			<div class="col-8-12">
-				<div class="contato-ciade">
-					<img src="img/logo-ciade.jpg">
-					<p class="ciade-descricao">O CIADE, Centro integrado de assistência social da assembléia de Deus é uma instituição social Com Sede própria, recém-construída e inaugurada em julho de 2011, com endereço localizado na Rua Abreulândia nº 77 com a Rua Esplendor, no bairro Planalto, onde atualmente estão abrigadas 22 senhoras. Estas instalações ocupam um terreno com 2.000m2, e uma área construída de 1.133,90 m2. O quadro funcional de 34 pessoas é composto por profissionais nas áreas de:</p>
+				<div class="col-4-12">
+					<div class="contato-ciade">
+						<img src="img/logo-ciade.jpg">
+					</div>
+				</div>
+				<div class="col-8-12">
+						<p class="ciade-descricao">O <strong>CIADE<strong>, Centro integrado de assistência social da assembléia de Deus é uma instituição social Com Sede própria, recém-construída e inaugurada em julho de 2011, com endereço localizado na Rua Abreulândia nº 77 com a Rua Esplendor, no bairro Planalto, onde atualmente estão abrigadas 22 senhoras. Estas instalações ocupam um terreno com 2.000m2, e uma área construída de 1.133,90 m2. O quadro funcional de 34 pessoas é composto por profissionais nas áreas de:</p>
+				</div>
+				<div class="col-1-1">
+					<h2>Parceiros</h2>
+					<ul class="parceiros">
+						<div class="col-1-5">
+							<li class="parceiro-logo" id="sabor_fino"></li>
+						</div>
+						<div class="col-1-5">
+							<li class="parceiro-logo" id="camara_cascudo"></li>
+						</div>	
+						<div class="col-1-5">
+							<li class="parceiro-logo" id="filmfactory"></li>
+						</div>	
+						<div class="col-1-5">
+							<li class="parceiro-logo" id="coletivo"></li>
+						</div>	
+						<div class="col-1-5">
+							<li class="parceiro-logo" id="varlik"></li>
+						</div>	
+					</ul>
 				</div>
 			</div>
+
 			<div class="col-4-12">
-				<div class="contato-fomulario">
-					<h2 class="contato-fomulario-titulo">Entre em <span class="contato-formulario-titulo-destaque">Contato</span> conosco</h2>
+				<h2 class="contato-fomulario-titulo">Entre em <span class="contato-formulario-titulo-destaque">Contato</span> conosco</h2>
+					<div class="contato-fomulario">
 					<form>
 						<label for="nome">Nome:</label>
 						<input type="text" name="nome" class="contato-formulario-input" placeholder="Ex.: João Maria">
@@ -97,7 +129,7 @@
 						<input type="email" name="email" class="contato-formulario-input" placeholder="Ex.: joao@mail.com">
 						<label for="mensagem">Mensagem:</label>
 						<textarea name="mensagem" class="contato-formulario-textarea" placeholder="Escreva sua mensagem aqui"></textarea>
-						<input type="submit" value="enviar">
+						<input type="submit" value="Enviar" align="right" class="contato-formulario-submit">
 					</form>
 				</div>
 			</div>
@@ -107,8 +139,8 @@
 	<footer>
 		<div class="grid">
 			<ul class="links">
-				<li class="links-item"><a href="http://facebook.com/redesecap">facebook</a></li>
-				<li class="links-item"><a href="http://facebook.com/redesecap">twitter</a></li>
+				<!-- <li class="links-item"><a href="http://facebook.com/redesecap">facebook</a></li>
+				<li class="links-item"><a href="http://facebook.com/redesecap">twitter</a></li> -->
 			</ul>
 		</div>
 	</footer>	
